9dc813c99f6955706406e4268354448c81b7c387
Malcolm Seyd <malcolmseyd@gmail.com>
2021-11-20 11:14:41 -0800

    get some views working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer, useState } from "react";
 
 function App() {
+
+  // [state, dispatch] = useReducer()
+
+  const state = {
+    gameID: "EXAMPLE-GAME-ID",
+    sessionID: "EXAMPLE-SESSION-ID",
+    state: null,
+  }
+
+  const route = "game:wait";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <hr />
+      {route === "home" &&
+        <HomePage {...state} />}
+      {route === "game:wait" &&
+        <GameWaitPage {...state} />}
+      {route === "game:play" &&
+        <GamePlayPage {...state} />}
+      {route === "game:over" &&
+        <GameOverPage {...state} />}
     </div>
   );
 }
 
+function HomePage() {
+  return (<div>
+    <div>
+      <button>Create Game</button>
+    </div>
+    <div>
+      <input placeholder="Room Code" />
+      <button>Join Game</button>
+    </div>
+  </div>
+  )
+
+}
+
+function CurrentGameID({ gameID }) {
+  return (<div>
+    Game code: <input value={gameID}></input>
+  </div>)
+}
+
+function GameWaitPage({ gameID }) {
+  return (<div>
+    <CurrentGameID gameID={gameID} />
+    <h2>Waiting for opponent</h2>
+  </div>)
+}
+
+function GamePlayPage({ gameID, state }) {
+  return (<div>
+    <CurrentGameID />
+    <ScoreBoard {...state} />
+    <Board {...state} />
+  </div>)
+}
+
+function ScoreBoard({ myWins, theirWins }) {
+  return (<div>
+    Wins: Me {myWins} - {theirWins} Them
+  </div>)
+}
+
+const boardStrings = new Map();
+boardStrings.set("x", "Ｘ");
+boardStrings.set("o", "Ｏ");
+boardStrings.set(null, "　");
+
+function Board({ board }) {
+  return (<div>
+    <table>
+      {board.map((row, rowIdx) => (
+        <tr>
+          {row.map((char, colIdx) => (
+            <td>
+              <span>{boardStrings.get(char)}</span>
+            </td>
+          ))}
+        </tr>
+      ))}
+    </table>
+  </div>)
+}
+
+const playingStrings = new Map();
+playingStrings.set("me", "Your");
+playingStrings.set("them", "Their")
+
+function Turn({ playing }) {
+  return (<div>
+    {playingStrings.set()}
+  </div>)
+}
+
+function GameOverPage() {
+
+}
+
+function Title() {
+  return (
+    <h1 style={{ textAlight: "center" }}>
+      UVEC Tic Tac Toe
+    </h1>
+  );
+}
 export default App;
